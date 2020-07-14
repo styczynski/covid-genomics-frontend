27d96ee5cc860760cf2a9cbecdb51a61936a6ab6
Piotr Styczyński <piotrs@radcode.co>
2020-07-14 19:57:54 +0200

    Initialize project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.less';
-import {Solver} from "./components/Solver/solver";
 
 function App() {
   return (
     <div className="App">
       <header className="AppHeader">
         <p>
-          SAT Solver <a href="https://github.com/styczynski/go-sat-solver">(view on Github)</a>
+
         </p>
         <Solver />
       </header>
--- src/components/Spinner/spinner.tsx
+++ src/components/Spinner/spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 
-export const SolverSpinner = () => {
+export const Spinner = () => {
     return (
         <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
     );
